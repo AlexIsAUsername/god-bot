51d7ef752a6a339ce2790a7af3500fadb829fd8b
Alex Yeoh <yeoh.3@wright.edu>
2024-09-13 09:45:16 -0400

    added comments and documentation, modified to not recompute markovify.Text if not necessary

--- markov-chain.py
+++ markov-chain.py
@@ -5,98 +5,18 @@ import numpy
 import random
 import time
 
-
 input_file = "kjvc.txt"
 chain_order = 4  # 4 is the magic number
 
-
-def get_last_n(lst, n):
-    return lst[-n:]
-
-
-def find_closest_word(bad_word: str, corpus: list[str]):
-
-    corpus = list(corpus)
-
-    closest_words = []
-
-    shortest_dist = 999  # init with high value
-
-    for cur_word in corpus:
-        cur_dist = levenshteinDistanceDP(bad_word, cur_word)
-
-        if cur_dist < shortest_dist:
-            closest_words = []  # clear currest best words
-            shortest_dist = cur_dist
-            closest_words.append(cur_word)
-        elif cur_dist == shortest_dist:
-            closest_words.append(cur_word)
-
-    return random.choice(closest_words)
-
-
-def get_user_prompt(the_bible: str) -> str:
-    global chain_order
-    set_corpus = set(the_bible.replace(".", "", -1).split(" "))
-    full_dirty_input = input("Enter prompt: ")
-    punc = ["[", "]", ":", ",", ";", "’", "—", "!", "(", ")", "?"]
-    for mark in punc:
-        full_dirty_input = full_dirty_input.replace(mark, "", -1)
-    full_dirty_input = full_dirty_input.strip().split(" ")
-    dirty_input = []
-
-    if len(full_dirty_input) > chain_order:
-        dirty_input = full_dirty_input[len(full_dirty_input) - chain_order :]
-    else:
-        dirty_input = full_dirty_input
-
-    bad_words = []
-    for word in dirty_input:
-        if word not in set_corpus:
-            bad_words.append(word)
-
-    good_words = []
-    for word in bad_words:
-        good_words.append(find_closest_word(word, set_corpus))
-
-    cleaned_input = []
-
-    index_in_good = 0
-
-    for i in range(len(dirty_input)):
-        if dirty_input[i] in bad_words:
-            cleaned_input.append(good_words[index_in_good])
-            index_in_good += 1
-        else:
-            cleaned_input.append(dirty_input[i])
-
-    # after levinsteine search
-    print("Cleaned input: ", cleaned_input)
-    temp = find_prompt_chain(the_bible.split(" "), cleaned_input)
-    print("Final prompt: ", temp)
-    return " ".join(temp)
-
-
-def convolve_lists(corpus: list[str], cleaned_input: list[str]):
-    global chain_order
-    reversed_input = []
-    max = count = 0
-    for i in range(len(cleaned_input) - 1, -1, -1):
-        reversed_input.append(cleaned_input[i])  # deep copy reverse input
-    for i in range(len(corpus) - 1, -1, -1):
-        if reversed_input[count] == corpus[i]:
-            count += 1
-            if count > max:
-                max = count
-                if max == len(cleaned_input):
-                    return cleaned_input
-        else:
-            count = 0
-
-    return cleaned_input[len(cleaned_input) - max :]
-
-
 def permutations(elements):
+    """permutations function taken from https://stackoverflow.com/a/104436
+
+    Args:
+        elements (_type_): _description_
+
+    Yields:
+        _type_: _description_
+    """
     if len(elements) <= 1:
         yield elements
         return
@@ -106,36 +26,16 @@ def permutations(elements):
             yield perm[:i] + elements[0:1] + perm[i:]
 
 
-def find_prompt_chain(corpus: list[str], cleaned_input: list[str]):
-    final_prompt = temp_prompt = []
-    permutated_input_list = list(permutations(cleaned_input))
-    for permutated_input in permutated_input_list:
-        temp_prompt = convolve_lists(corpus, permutated_input)
-        if len(temp_prompt) > len(final_prompt):
-            final_prompt = temp_prompt
-            if len(final_prompt) == len(cleaned_input):
-                return final_prompt
-    return final_prompt
-    # return convolve_lists(corpus, cleaned_input)
-
-
-def response(model: markovify.Text, prompt: str):
-    global chain_order
-
-    usable_words = get_last_n(prompt.split(" "), chain_order)
-
-    real_prompt = " ".join(usable_words)
-
-    if len(real_prompt) > 0:
-        temp = tuple(prompt.split(" "))
-        print("Initial state: ", temp)
-        return model.make_sentence(init_state=temp)
-        # return model.make_sentence_with_start(real_prompt, strict=False)
-    else:
-        return "Prompt too short :("
-
-
 def levenshteinDistanceDP(token1, token2):
+    """levenshtein distance function taken from https://blog.paperspace.com/implementing-levenshtein-distance-word-autocomplete-autocorrect/
+
+    Args:
+        token1 (_type_): _description_
+        token2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     distances = numpy.zeros((len(token1) + 1, len(token2) + 1))
 
     for t1 in range(len(token1) + 1):
@@ -167,25 +67,178 @@ def levenshteinDistanceDP(token1, token2):
     return distances[len(token1)][len(token2)]
 
 
-def inject_prompt(
-    prompt: str, the_bible: str
-) -> str:  # TODO decide if its worth killing
+def get_user_prompt(the_bible: str) -> str:
+    """gets user input and cleans it by removing punctuation, 
+    converting the input to words in the_bible (uses find_closest_word), 
+    and the longest contiguous string of those words in the_bible (uses find_prompt_chain)
 
-    prompt += " amen"
-    bible_words = the_bible.split(".")
-    index = random.randrange(len(bible_words))
+    Args:
+        the_bible (str): text that will turn into the markov chain, preprocessed by kjv-clean.py
 
-    new_testament = [prompt] + bible_words[index:-1] + bible_words[0:index]
-    # bible_words.insert(index, prompt)
+    Returns:
+        str: fully cleaned initial state for markovify
+    """
+    global chain_order
+    # makes a set of all the words in the_bible
+    set_corpus = set(the_bible.replace(".", "", -1).split(" "))
+    full_dirty_input = input("Enter prompt: ")
+    punc = ["[", "]", ":", ",", ";", "’", "—", "!", "(", ")", "?"]
+    # strips the input of punctuation
+    for mark in punc:
+        full_dirty_input = full_dirty_input.replace(mark, "", -1)
+    full_dirty_input = full_dirty_input.strip().split(" ")
+    
+    dirty_input = []
 
-    return ". ".join(new_testament)
+    # keeps only the last chain_order number of words from the input
+    if len(full_dirty_input) > chain_order:
+        dirty_input = full_dirty_input[len(full_dirty_input) - chain_order :]
+    else:
+        dirty_input = full_dirty_input
+
+    bad_words = []
+    
+    # tracks all words that are not in the_bible
+    for word in dirty_input:
+        if word not in set_corpus:
+            bad_words.append(word)
+
+    good_words = []
+    
+    # tracks all the closest replacement words for bad_words from the words in the_bible
+    for word in bad_words:
+        good_words.append(find_closest_word(word, set_corpus))# levenshtein Distance search the closest words in the bible
+
+    cleaned_input = []
+    index_in_good = 0
+
+    # replace all bad words with good words and store in cleaned_input
+    for i in range(len(dirty_input)):
+        if dirty_input[i] in bad_words:
+            cleaned_input.append(good_words[index_in_good])
+            index_in_good += 1
+        else:
+            cleaned_input.append(dirty_input[i])
+
+    # returns longest chain of fully cleaned input
+    print("Cleaned input: ", cleaned_input)
+    temp = find_prompt_chain(the_bible.split(" "), cleaned_input) # finds longest chain of cleaned_input
+    print("Final prompt: ", temp)
+    return " ".join(temp)
+
+
+def find_closest_word(bad_word: str, corpus: list[str]) -> str:
+    """uses levenshteinDistanceDP to find the most similar word in the corpus for a given bad_word
+
+    Args:
+        bad_word (str): word not in corpus
+        corpus (list[str]): list of valid words
+
+    Returns:
+        str: valid word most similar to bad_word
+    """
+    corpus = list(corpus)
+
+    closest_words = []
+
+    shortest_dist = 999  # init with high value
+
+    # loop to check how similar each valid word is to the bad_word
+    for cur_word in corpus:
+        cur_dist = levenshteinDistanceDP(bad_word, cur_word)
+
+        if cur_dist < shortest_dist:
+            closest_words = []  # clear currest best words
+            shortest_dist = cur_dist
+            closest_words.append(cur_word)
+        elif cur_dist == shortest_dist:
+            closest_words.append(cur_word)
+
+    return random.choice(closest_words)
+
+
+def find_prompt_chain(corpus: list[str], cleaned_input: list[str])-> list[str]:
+    """permutes the list of cleaned_input to find the longest contiguous chain 
+    that appears in corpus with a preference for the original cleaned_input
+
+    Args:
+        corpus (list[str]): the starting text
+        cleaned_input (list[str]): chain of words that exist in corpus
+
+    Returns:
+        list[str]: longest contiguous chain of cleaned_input words
+    """
+    final_prompt = temp_prompt = []
+    permutated_input_list = list(permutations(cleaned_input))
+    for permutated_input in permutated_input_list:
+        temp_prompt = convolve_lists(corpus, permutated_input)
+        if len(temp_prompt) > len(final_prompt):
+            final_prompt = temp_prompt
+            if len(final_prompt) == len(cleaned_input):
+                return final_prompt
+    return final_prompt
+    # return convolve_lists(corpus, cleaned_input)
+
+
+def convolve_lists(corpus: list[str], cleaned_input: list[str]) ->list[str]:
+    """performs convolution on corpus and cleaned_input to determine the 
+    longest contiguous chain of cleaned_input that appears in corpus
+
+    Args:
+        corpus (list[str]): starting text
+        cleaned_input (list[str]): list of words that appear in corpus
+
+    Returns:
+        list[str]: longest chain of cleaned_input in corpus
+    """
+    global chain_order
+    reversed_input = []
+    max = count = 0
+    for i in range(len(cleaned_input) - 1, -1, -1):
+        reversed_input.append(cleaned_input[i])  # deep copy reverse input
+    for i in range(len(corpus) - 1, -1, -1):
+        if reversed_input[count] == corpus[i]:
+            count += 1
+            if count > max:
+                max = count
+                if max == len(cleaned_input):
+                    return cleaned_input
+        else:
+            count = 0
+
+    return cleaned_input[len(cleaned_input) - max :]
+
+
+def response(model: markovify.Text, prompt: str) -> str:
+    """generate the the response from the markov chain
+
+    Args:
+        model (markovify.Text): markov chain
+        prompt (str): initial state of the markov chain
+
+    Returns:
+        str: response from the markov chain
+    """
+    global chain_order
+
+    if len(prompt) > 0:
+        temp = tuple(prompt.split(" "))
+        print("Initial state: ", temp)
+        return model.make_sentence(init_state=temp)
+        # return model.make_sentence_with_start(real_prompt, strict=False)
+    else:
+        return "Prompt too short :("
 
 
 def printer(text: str) -> None:
+    """prints each character of the markov chain response
+
+    Args:
+        text (str): markov chain response
+    """
     for char in text:
         print(char, end="", flush=True)
         time.sleep(0.05)
-
     print()  # newline
 
 
@@ -195,18 +248,22 @@ def main():
     with open(input_file) as f:
         the_bible = f.read().replace("\n", " ", -1)
 
+    curr_state_size = 0
     while True:
         prompt = get_user_prompt(the_bible)
 
-        text_model = markovify.Text(
-            the_bible,
-            state_size=min(chain_order, len(prompt.split(" "))),
-        )
+        # determine the next state size and only recompute markov chain if its different
+        next_state_size = min(chain_order, len(prompt.split(" ")))
+        if(curr_state_size != next_state_size):
+            text_model = markovify.Text(
+                the_bible,
+                state_size=next_state_size,
+            )
+            curr_state_size = next_state_size
+
         res = response(text_model, prompt)
-        tts = gTTS(text=res, lang="en", slow=False)
-        tts.save("god.mp3")
-        p = vlc.MediaPlayer("god.mp3")
-        p.play()
+        gTTS(text=res, lang="en", slow=False).save("god.mp3") # properties of the text to speech audio file and save
+        vlc.MediaPlayer("god.mp3").play() # play the generated audio file
         printer(" ".join(res.split(" ")))
 
 
